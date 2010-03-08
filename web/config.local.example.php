f4d00382ee247d9caaa1d8842483a8c8fc98d2ae
Lars Olesen <lars@intraface.dk>
2010-03-08 09:39:04 +0000

    Added config.example.

---
+++ web/config.local.example.php
@@ -0,0 +1,8 @@
+<?php
+define('PATH_INCLUDE', dirname(__FILE__) . '/../' . PATH_SEPARATOR . get_include_path());
+
+
+$GLOBALS["intraface_private_key"] = 'Uzt6hhXRS9zrsu8JvdBmh1MdkwaaFSQgg15p8S5IJkYSFNiZIJb';
+$GLOBALS["intraface_site_id"] = 41;
+
+$GLOBALS["template_dir"] = dirname(__FILE__) . '/../VIH/Kursuscenter/templates/';
