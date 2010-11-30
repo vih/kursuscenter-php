2c5f6f7e1e96d12bda0d901b7a7ba218d1a3f7c2
Lars Olesen <lars@intraface.dk>
2010-11-30 09:27:43 +0100

    Makes sure that the file uri isset.

--- VIH/Kursuscenter/templates/page.tpl.php
+++ VIH/Kursuscenter/templates/page.tpl.php
@@ -1,4 +1,6 @@
-<img src="<?php e($picture['file_uri']); ?>" width="<?php e($picture['width']); ?>" height="<?php e($picture['height']); ?>" alt="" />
+<?php if (!empty($picture['file_uri'])): ?>
+    <img src="<?php e($picture['file_uri']); ?>" width="<?php e($picture['width']); ?>" height="<?php e($picture['height']); ?>" alt="" />
+<?php endif; ?>
 
 <h1><?php e($headline); ?></h1>
 
